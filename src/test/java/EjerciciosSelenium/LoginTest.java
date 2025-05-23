2b5eb6dab1bdbf65608003ed22daed8b69b79216
AyelenAG26 <aag260401@gmail.com>
2025-05-22 18:45:58 -0600

    Clase22may

---
+++ src/test/java/EjerciciosSelenium/LoginTest.java
@@ -0,0 +1,54 @@
+package EjerciciosSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+public class LoginTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+        WebElement usertxt=driver.findElement(By.xpath("//input[@id='inputUsername']"));
+        WebElement pwdtxt=driver.findElement(By.xpath("//input[@name='inputPassword']"));
+        WebElement loginbtn=driver.findElement(By.xpath("//button[text()='Sign In']"));
+
+
+        //Testeo con datos correctos y mensaje de exito
+       /* usertxt.sendKeys("Ayelen");
+        Thread.sleep(1000);
+        pwdtxt.sendKeys("rahulshettyacademy");
+        Thread.sleep(1000);
+        loginbtn.click();
+        Thread.sleep(2000);
+
+        WebElement successtxt=driver.findElement(By.xpath("//h1"));
+        System.out.println(successtxt.getText());
+        */
+
+        //Testeo con datos incorrectos y mensaje de error
+        usertxt.sendKeys("Ayelen");
+        Thread.sleep(1000);
+        pwdtxt.sendKeys("rahulshetty");
+        Thread.sleep(1000);
+        loginbtn.click();
+        Thread.sleep(4000);
+
+        WebElement errortxt=driver.findElement(By.xpath(" //p[@class='error']"));
+        System.out.println(errortxt.getText());
+
+
+
+
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
+
+    }
+}
---
+++ src/test/java/EjerciciosSelenium/TesteoCucumber.java
@@ -0,0 +1,31 @@
+package EjerciciosSelenium;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+public class TesteoCucumber {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+
+        WebElement donebtn=driver.findElement(By.xpath("//h4[contains(text(),'Cucumber')]/../div/button"));
+        WebElement countxt=driver.findElement(By.xpath("//h4[contains(text(),'Cucumber')]/../div[@class='stepper-input']/input"));
+        WebElement addbtn=driver.findElement(By.xpath("//h4[contains(text(),'Cucumber')]/../div[@class='stepper-input']/a[@class='increment']"));
+
+
+        addbtn.click();
+        Thread.sleep(1000);
+        addbtn.click();
+        Thread.sleep(1000);
+        addbtn.click();
+        Thread.sleep(2000);
+        donebtn.click();
+
+        System.out.println("Agrego : "+countxt.getText()+ "productos");
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+
+}
